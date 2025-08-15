9fc92f6ef4527e4643e11300775ed6649e439f88
Amanda Camacho <mandi94development@gmail.com>
2025-08-14 20:19:04 -0500

    Finish and testing

--- test/tasks.test.ts
+++ test/tasks.test.ts
@@ -4,24 +4,33 @@ import taskRoutes from "../src/routes/task";
 import authRoutes from "../src/routes/auth";
 import { UserModel } from "../src/models/user";
 import { dbConnect, dbDisconnect } from "./setupTest";
-import { fakeAuthMiddleware } from "./setupMiddleware";
+import bcrypt from "bcryptjs";
 
 let userId: string;
-const app = express();
-app.use(express.json());
+let token: string;
+
+const appAuth = express();
+appAuth.use(express.json());
+appAuth.use("/api/auth", authRoutes);
+appAuth.use("/api/tasks", taskRoutes);
 
 beforeAll(async () => {
   await dbConnect();
 
+  const hashedPassword = await bcrypt.hash("testpass123", 10);
   const user = await UserModel.create({
     name: "Task Tester",
     email: "taskuser@example.com",
-    password: "testpass123",
+    password: hashedPassword,
   });
   userId = user._id.toString();
 
-  // Using fake midleware
-  app.use("/api/tasks", fakeAuthMiddleware(userId), taskRoutes);
+  const res = await request(appAuth).post("/api/auth/login").send({
+    email: "taskuser@example.com",
+    password: "testpass123",
+  });
+  token = res.body.token;
+  if (!token) throw new Error("Token is undefined, login failed!");
 });
 
 afterAll(async () => {
@@ -30,54 +39,61 @@ afterAll(async () => {
 
 describe("Task Routes", () => {
   test("GET /api/tasks should return 401 if not authenticated", async () => {
-    const response = await request(app).get("/api/tasks");
+    const response = await request(appAuth).get("/api/tasks");
     expect(response.status).toBe(401);
   });
 
   test("POST /api/tasks should create a new task", async () => {
-    const res = await request(app).get("/api/tasks");
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body.data)).toBe(true);
-  });
+    const res = await request(appAuth)
+      .post("/api/tasks")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Task for POST test",
+        description: "This task is created just for POST test",
+        status: "pending",
+      });
 
-  test("GET /api/tasks should return paginated tasks", async () => {
-    const response = await request(app).get("/api/tasks?page=1&limit=10");
-
-    expect(response.status).toBe(200);
-    expect(Array.isArray(response.body.data)).toBe(true);
-    expect(response.body.pagination).toHaveProperty("total");
-    expect(response.body.pagination).toHaveProperty("currentPage");
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("_id");
   });
 
   test("GET /api/tasks/:id should return the created task", async () => {
-    const taskRes = await request(app).post("/api/tasks").send({
-      title: "Task for GET test",
-      description: "This task is created just for the GET/:id test",
-      status: "pending",
-    });
+    const taskRes = await request(appAuth)
+      .post("/api/tasks")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Task for GET/:id test",
+        description: "This task is created just for GET/:id test",
+        status: "pending",
+      });
 
     const taskId = taskRes.body._id;
 
-    const response = await request(app).get(`/api/tasks/${taskId}`);
+    const response = await request(appAuth)
+      .get(`/api/tasks/${taskId}`)
+      .set("Authorization", `Bearer ${token}`);
 
-    // 3️⃣ Validar
     expect(response.status).toBe(200);
     expect(response.body._id).toBe(taskId);
-    expect(response.body.title).toBe("Task for GET test");
+    expect(response.body.title).toBe("Task for GET/:id test");
     expect(response.body.status).toBe("pending");
   });
 
   test("PUT /api/tasks/:id should update the task", async () => {
-    const taskRes = await request(app).post("/api/tasks").send({
-      title: "Task for GET test",
-      description: "This task is created just for the GET/:id test",
-      status: "pending",
-    });
+    const taskRes = await request(appAuth)
+      .post("/api/tasks")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Task for PUT test",
+        description: "This task is created just for PUT test",
+        status: "pending",
+      });
 
     const taskId = taskRes.body._id;
 
-    const response = await request(app)
+    const response = await request(appAuth)
       .put(`/api/tasks/${taskId}`)
+      .set("Authorization", `Bearer ${token}`)
       .send({ status: "completed" });
 
     expect(response.status).toBe(200);
@@ -85,14 +101,22 @@ describe("Task Routes", () => {
   });
 
   test("DELETE /api/tasks/:id should delete the task", async () => {
-    const taskRes = await request(app).post("/api/tasks").send({
-      title: "Task for GET test",
-      description: "This task is created just for the GET/:id test",
-      status: "pending",
-    });
+    const taskRes = await request(appAuth)
+      .post("/api/tasks")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        title: "Task for DELETE test",
+        description: "This task is created just for DELETE test",
+        status: "pending",
+      });
 
     const taskId = taskRes.body._id;
-    const response = await request(app).delete(`/api/tasks/${taskId}`);
+
+    const response = await request(appAuth)
+      .delete(`/api/tasks/${taskId}`)
+      .set("Authorization", `Bearer ${token}`);
+
     expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("message", "Task deleted");
   });
 });
